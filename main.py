cefcb758320a775f042548b0aa221b5257d1e824
Katherine Austin (laptop) <kval148@wgu.edu>
2026-03-06 12:07:29 -0800

    main.py Change train_random_forest output_artifact name to "random_forest_export"

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def go(config: DictConfig):
                     "stratify_by": config["modeling"]["stratify_by"],
                     "rf_config": rf_config,
                     "max_tfidf_features": config["modeling"]["max_tfidf_features"],
-                    "output_artifact": "model_export"
+                    "output_artifact": "random_forest_export"
                 }
             )
 
